1a08eaba607371ab66f20861b01d278332ff492d
Adil <adilforms@gmail.com>
2018-12-10 11:27:22 -0500

    removed steps funcitons

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -3,34 +3,22 @@ def call(Map config) {
     pipeline {
      agent any
         
-stages {
+
 stage('Checkout'){
-  steps{
-      
+  
    checkout scm
- 
-      
-  }
 }
 stage('Build'){
-  steps {
+
     echo 'building'
    sh 'npm config set unsafe-perm=true'
    sh 'npm install'
-  }
 }
 stage('Test'){ 
-    steps {
-  
-   echo 'version'
-    
-  
-  }
-  
+    echo 'version'  
 }
-    stage('Publish') {
-  
-   def environment = branch_to_environment()[env.BRANCH_NAME]
+stage('Publish') {  
+ def environment = branch_to_environment()[env.BRANCH_NAME]
    if(environment != null) {
     try {
      if (environment ==' master') {
