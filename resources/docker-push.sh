5ec2e0ec929da0b57f5515c781fa348937f1ac12
Adil <adilforms@gmail.com>
2018-12-11 15:02:54 -0500

    echo branch name

--- resources/docker-push.sh
+++ resources/docker-push.sh
@@ -1,6 +1,7 @@
     echo 'publishing'
     curl https://raw.githubusercontent.com/adilforms/Shared-tempalte/test/Dockerfile > Dockerfile
    version=$(git rev-parse --short HEAD)
+   echo $BRANCH_NAME
    P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
    Package_Name=${P_Name//$BRANCH_NAME/''}
    docker build -t adilforms/$Package_Name$version:$BRANCH_NAME .
