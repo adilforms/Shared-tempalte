6f02acf63db56dd2378b4981810a63aebb104979
Adil <adilforms@gmail.com>
2018-12-11 10:35:14 -0500

    loadconfigs

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -1,7 +1,8 @@
 def call(Map config) {
  node('master'){
+  stage('load-configs'){
   env.file = shell(readFileFromWorkspace('dockerpush.sh')
-        
+                   }
 stage('Checkout'){
   
    checkout scm
