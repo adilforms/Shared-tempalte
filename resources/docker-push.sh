556b2ce54936374480b48501fd616dd7fc46affb
Adil <adilforms@gmail.com>
2018-12-11 14:28:46 -0500

    removed sh ''

--- resources/docker-push.sh
+++ resources/docker-push.sh
@@ -1,4 +1,3 @@
-sh '''#!/bin/bash -el
     echo 'publishing'
     curl https://raw.githubusercontent.com/adilforms/Shared-tempalte/test/Dockerfile > Dockerfile
    version=$(git rev-parse --short HEAD)
@@ -6,4 +5,4 @@ sh '''#!/bin/bash -el
    Package_Name=${P_Name//$BRANCH_NAME/''}
    docker build -t adilforms/$Package_Name$version:$BRANCH_NAME .
    docker push adilforms/$Package_Name$version:$BRANCH_NAME
-   '''
+  
