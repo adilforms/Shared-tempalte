13f283529851fcd53c475fe02b09fcd142ca2816
Adil <adilforms@gmail.com>
2018-12-11 09:52:28 -0500

    replace - .

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -25,7 +25,7 @@ stage('Publish') {
 sh '''#!/bin/bash -el
     echo 'publishing'
    version=$(git rev-parse --short HEAD)
-   P_Name=$(echo $JOB_NAME | tr / - | tr "[:upper:]" "[:lower:]")
+   P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
    Package_Name=${P_Name//$BRANCH_NAME/''}
    
       
