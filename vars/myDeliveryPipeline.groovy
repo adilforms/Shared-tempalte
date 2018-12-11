f99e8b2d2b50eecdbff70b5a3d6602664a7cfe0f
Adil <adilforms@gmail.com>
2018-12-11 10:17:10 -0500

    testing inject file

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -1,4 +1,4 @@
- 
+def content = readfile  ./dockerpush.sh 
 def call(Map config) {
  node('master'){
         
@@ -20,19 +20,9 @@ stage('Test'){
 }
 stage('Publish') { 
      
+   echo 'Publishing'
    
-   
-sh '''#!/bin/bash -el
-    echo 'publishing'
-   version=$(git rev-parse --short HEAD)
-   P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
-   Package_Name=${P_Name//$BRANCH_NAME/''}
-   
-      
-    
-   docker build -t adilforms/$Package_Name$version:$BRANCH_NAME .
-   docker push adilforms/$Package_Name$version:$BRANCH_NAME
-   '''
+
    
 }
 
