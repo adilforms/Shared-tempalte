8fcb97c705dc3a42109ebfd5c013f0a1b0e6036a
Adil <adilforms@gmail.com>
2018-12-11 15:32:03 -0500

    Typo $

--- resources/docker-push.sh
+++ resources/docker-push.sh
@@ -4,5 +4,5 @@
    PNAME=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
    PACKAGENAME=${PNAME%.*}
    docker build -t adilforms/$PACKAGENAME.$version:$BRANCH_NAME .
-   docker push adilforms/$$PACKAGENAME.$version:$BRANCH_NAME
+   docker push adilforms/$PACKAGENAME.$version:$BRANCH_NAME
   
