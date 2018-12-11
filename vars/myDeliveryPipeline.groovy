43f8d953c373ccd5b485132149d483f19318da39
Adil <adilforms@gmail.com>
2018-12-11 14:12:33 -0500

    testing Copy Clobal funtion

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -16,15 +16,7 @@ stage('Test'){
 stage('Publish') { 
      
  
-sh '''#!/bin/bash -el
-    echo 'publishing'
-    curl https://raw.githubusercontent.com/adilforms/Shared-tempalte/test/Dockerfile > Dockerfile
-   version=$(git rev-parse --short HEAD)
-   P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
-   Package_Name=${P_Name//$BRANCH_NAME/''}
-   docker build -t adilforms/$Package_Name$version:$BRANCH_NAME .
-   docker push adilforms/$Package_Name$version:$BRANCH_NAME
-   '''
+sh(copyGlobalLibraryScript('resources/docker-push.sh')
  }
 
 }
