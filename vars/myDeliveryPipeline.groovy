856f7a31c39ea34ab2ed6c7b17ea1d0efa42d91a
Adil <adilforms@gmail.com>
2018-12-11 14:06:07 -0500

    Adding shell step to call Dockerfile

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -23,11 +23,16 @@ stage('Test'){
 }
 stage('Publish') { 
      
-   echo 'Publishing' 
- echo 'Injecting Dockerfile in Workspace'
- @library('docker-push.sh')
-
- echo 'Executing shell script to push package to Docker'
+ 
+sh '''#!/bin/bash -el
+    echo 'publishing'
+    curl https://raw.githubusercontent.com/adilforms/Shared-tempalte/test/Dockerfile > Dockerfile
+   version=$(git rev-parse --short HEAD)
+   P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
+   Package_Name=${P_Name//$BRANCH_NAME/''}
+   docker build -t adilforms/$Package_Name$version:$BRANCH_NAME .
+   docker push adilforms/$Package_Name$version:$BRANCH_NAME
+   '''
  }
 
 }
