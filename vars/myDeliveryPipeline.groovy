23910616baf6f14fd28cd272986b07b098b8a64d
Adil <adilforms@gmail.com>
2018-12-12 00:17:25 -0500

    removed failure

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -22,13 +22,10 @@ def request = libraryResource 'docker-push.sh'
   stage('PostAction') {
    echo "Cleaning Work Space"
     sh 'rm -rf *'
-  
-    failure {
+ 
         mail to: 'team@example.com',
              subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
-             body: "Something is wrong with ${env.BUILD_URL}"
-    }
-
+             body: "Something is wrong with ${env.BUILD_URL}
  
  }
 
