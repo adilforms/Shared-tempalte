26172da0e66d884ad28337b31f63994658c9f088
Adil <adilforms@gmail.com>
2018-12-12 00:04:17 -0500

    Adding Post Action

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -23,6 +23,13 @@ def request = libraryResource 'docker-push.sh'
    echo "Cleaning Work Space"
     sh 'rm -rf *'
   }
+  post {
+    failure {
+        mail to: 'team@example.com',
+             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             body: "Something is wrong with ${env.BUILD_URL}"
+    }
+}
  
  }
 
