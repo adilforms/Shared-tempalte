36f5b54ffbe379ed42e25fe84f2d38fd33c0ce32
Adil <adilforms@gmail.com>
2018-12-12 00:18:47 -0500

    mail

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -23,9 +23,9 @@ def request = libraryResource 'docker-push.sh'
    echo "Cleaning Work Space"
     sh 'rm -rf *'
  
-        mail to: 'team@example.com',
+        mail to: 'emad.syed@careerbuilder.com',
              subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
-             body: "Something is wrong with ${env.BUILD_URL}
+             body: "Something is wrong with $BUILD_URL
  
  }
 
