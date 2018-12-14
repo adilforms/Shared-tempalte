bd2a655bc04d2fa3e4c3dd1aad6f3b0a9537470b
Adil <adilforms@gmail.com>
2018-12-14 00:49:33 -0500

    Testing Try and Catch funtion

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -6,31 +6,55 @@ def call(Map config) {
 stage('Checkout'){
  checkout scm 
 }
-
+   catch(e){
+    echo 'failed in Checkout'
+    return false
+   }
 stage('Build'){
-
+ try {
     echo 'building'
     sh 'npm install'
- 
+ }
+ catch(e){
+  return false
+  echo 'failed in Build'
+ }
 }
 stage('Test'){ 
+ try {
     echo 'version' 
  sh ' sudo cd /'
+ }
+ catch(e) {
+  return false
+  echo 'failed in Test'
+ }
+ 
 
 }
 stage('Publish') { 
-
+ try {
 def request = libraryResource 'docker-push.sh'
  sh request
  }
+ catch(e){
+  return false
+  echo 'failed in Publish'
+ }
+ }
    stage('PostAction') {
+    try {
    echo "Cleaning Work Space"
     deleteDir()  
+    }
+    catch(e){
+     echo 'failed in Post Action'
+     return false
+    }
   }
-  }
-  catch(e){
-   return false
-  }
+ 
+  
+
   
   
 }
