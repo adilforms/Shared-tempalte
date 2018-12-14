67a67dc28046375ef2e1fd089df0f8e1a6de79b1
Adil <adilforms@gmail.com>
2018-12-14 01:22:12 -0500

    Testing Catch error

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -4,64 +4,25 @@ def call(Map config) {
   try{
   
 stage('Checkout'){
- try{
  checkout scm 
 }
- catch(e){
-    return false
-   echo "system-check-flow failed"
-  currentBuild.result = 'FAILURE'
-   }
-}
 stage('Build'){
- try {
     echo 'building'
     sh 'npm install'
- }
- catch(e){
-
-  echo 'failed in Build'
-   echo "system-check-flow failed"
-  currentBuild.result = 'FAILURE'
- }
 }
 stage('Test'){ 
- try {
     echo 'version' 
- sh ' sudo cd /'
-  build job: 'system-check-flow'
- }
- catch(e) {
- 
-  echo 'failed in Test'
-   echo "system-check-flow failed"
-  currentBuild.result = 'FAILURE'
  }
  
 
 }
 stage('Publish') { 
- try {
 def request = libraryResource 'docker-push.sh'
  sh request
  }
- catch(e){
- 
-  echo 'failed in Publish'
-   echo "system-check-flow failed"
-  currentBuild.result = 'FAILURE'
- }
- }
-   stage('PostAction') {
-    try {
+stage('PostAction') {
    echo "Cleaning Work Space"
     deleteDir()  
-    }
-    catch(e){
-     echo 'failed in Post Action'
-     echo "system-check-flow failed"
-  currentBuild.result = 'FAILURE'
-    }
   }
  
   build job: 'system-check-flow'
