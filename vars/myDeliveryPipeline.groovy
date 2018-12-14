a3bf0ee12308298d1b2cc1f363732c181aeca7ea
Adil <adilforms@gmail.com>
2018-12-14 01:29:18 -0500

    removed
      build job: 'system-check-flow'
       currentBuild.result = 'SUCCESS'
      }
      catch (err){
          echo "system-check-flow failed"
      currentBuild.result = 'FAILURE'
       }

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -22,13 +22,11 @@ stage('PostAction') {
    echo "Cleaning Work Space"
     deleteDir()  
   }
- 
-  build job: 'system-check-flow'
-   currentBuild.result = 'SUCCESS'
+
   }
   catch (err){
       echo "system-check-flow failed"
-  currentBuild.result = 'FAILURE'
+  
    }
 
   
