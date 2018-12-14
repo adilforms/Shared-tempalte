b7962c5b0ef62c1eeb0813bebfe0dbc8c92bb67d
Adil <adilforms@gmail.com>
2018-12-14 01:30:53 -0500

    Current Build test

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -22,11 +22,13 @@ stage('PostAction') {
    echo "Cleaning Work Space"
     deleteDir()  
   }
+   build job: 'system-check-flow'
+   currentBuild.result = 'SUCCESS'
 
   }
   catch (err){
       echo "system-check-flow failed"
-  
+   currentBuild.result = 'FAILURE'
    }
 
   
